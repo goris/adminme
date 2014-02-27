612a9533d61487d675564e1e69288cbeebfb9dfc
David Tovar Goris <sgoris@gmail.com>
2014-02-26 21:36:11 -0600

    Debts are scaffolded

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Adminme::Application.routes.draw do
+  resources :debts
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
